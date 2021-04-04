a240eef403467dd61525c7af519b4e8bc8df19ac
UnderAnder <andryushin.alexander@gmail.com>
2021-04-04 20:02:35 +0700

    Stage 4/7: Choose your path

--- university.py
+++ university.py
@@ -1,11 +1,23 @@
+from copy import copy
+
 applicants = []
-total, max_accepted = int(input()), int(input())
-for i in range(total):
-    line = input()
-    applicants.append(line.split())
+departments = {'Mathematics': [], 'Physics': [], 'Biotech': [], 'Chemistry': [], 'Engineering': []}
+max_accepted = int(input())
+with open('applicants.txt') as f:
+    for line in f:
+        applicants.append(line.split())
 
 applicants_sorted = sorted(applicants, key = lambda x: (-float(x[2]), x[0], x[1]))
 
-print('Successful applicants:')
-for i in applicants_sorted[:max_accepted]:
-    print(i[0], i[1])
+for i in range (3, 6):  # priority fields in file
+    for applicant in copy(applicants_sorted):
+        if len(departments[applicant[i]]) < max_accepted:
+            departments[applicant[i]].append(applicant)
+            applicants_sorted.remove(applicant)
+
+
+for department in sorted(departments.keys()):
+    print(department)
+    for student in sorted(departments[department], key = lambda x: (-float(x[2]), x[0], x[1])):
+        print(*student[:3])
+    print()
