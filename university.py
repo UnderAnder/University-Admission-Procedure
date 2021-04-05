bfc20d771cc47bf5b4c1c33d59315e978adbdfa3
UnderAnder <andryushin.alexander@gmail.com>
2021-04-05 15:25:23 +0700

    Stage 7/7: Something special
        Read an N integer from the input. This integer represents the maximum number of students for each department.
        Read the file named applicants.txt once again. Mind one additional column, right after the last exam's result. This column represents the special exam's score. For example, Willie McBride 76 45 79 80 100 Physics Engineering Mathematics(where 100 is the admission exam's score).
        Choose the best score for a student in the ranking: either the mean score for the final exam(s) or the special exam's score. Use the same set of finals for each Department as in the previous stage. Note that you may need to compare the values several times: for example, if a student doesn't get accepted to the Department of the first priority, compare the finals mean score and the special exam's score once again (but this time, for the second priority department).
        Keep the rest of the steps the same as in the previous stage. Once again, there should be no more than N accepted applicants for each department; use the same principles for sorting.
        Output the names and the student's best score, either the mean finals score or the special exam's score to the files.

--- university.py
+++ university.py
@@ -6,12 +6,12 @@ max_accepted = int(input())
 with open('applicants.txt') as f:
     applicants = [line.split() for line in f]
 
-for i in range(6, 9):  # priority fields in input file
+for i in range(7, 10):  # priority fields in input file
     for dep in departments.keys():
-        applicants_sorted = sorted(applicants, key=lambda x: (-(int(x[exam[dep][0]])+int(x[exam[dep][1]])), x[0], x[1]))
-        for applicant in applicants_sorted:
+        sort_key = lambda x: (-max(int(x[exam[dep][0]]) + int(x[exam[dep][1]]), float(x[6]) * 2), x[0], x[1])
+        for applicant in sorted(applicants, key=sort_key):
             if applicant[i] == dep and len(departments[dep]) < max_accepted:
-                score = (int(applicant[exam[dep][0]]) + int(applicant[exam[dep][1]])) / 2
+                score = max(((int(applicant[exam[dep][0]]) + int(applicant[exam[dep][1]])) / 2), float(applicant[6]))
                 departments[dep].append([applicant[0], applicant[1], score])
                 applicants.remove(applicant)
 
